c0bda4b092fdbeeb3406a558e7be575fcd87299c
rysk1013 <rysk2908@gmail.com>
2021-07-12 11:00:21 +0900

    loop

--- ruby3.rb
+++ ruby3.rb
@@ -25,3 +25,24 @@ while num1.to_s != num1.to_s.reverse do
  p num1
 end
 puts num1
+
+x, y, z = 1000, 1300, 1500
+days = 4
+time = [[0, 9], [9, 17], [17, 22], [22, 23]]
+sum = 0
+
+(0..days-1).each do |i|
+    (time[i][0]+1..time[i][1]).each do |t|
+        if t <= 9
+           sum += z
+        elsif t <= 17
+           sum += x        
+        elsif t <= 22
+           sum += y
+        else
+           sum += z
+        end
+    end
+end
+
+puts sum
