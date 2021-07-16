37d6c5f9577a1032d8aefd2a26f5198d15018504
rysk1013 <rysk2908@gmail.com>
2021-07-16 09:07:05 +0900

    loop

--- ruby3.rb
+++ ruby3.rb
@@ -30,7 +30,6 @@ x, y, z = 1000, 1300, 1500
 days = 4
 time = [[0, 9], [9, 17], [17, 22], [22, 23]]
 sum = 0
-
 (0..days-1).each do |i|
     (time[i][0]+1..time[i][1]).each do |t|
         if t <= 9
@@ -44,5 +43,31 @@ sum = 0
         end
     end
 end
-
 puts sum
+
+# リーグ表結果出力
+teams = 3
+m = teams * (teams - 1) / 2
+results = [[1,3], [2, 1], [2,3]]
+rows = []
+results = results.sort
+teams.times do |i|
+    row = []
+    teams.times do |j|
+       if i == j
+           row << '-'
+       elsif results[i][0] == i+1 && results[i][1] == j+1 || results.include?([i+1, j+1])
+           row << 'W'
+       else
+           row << 'L'
+       end
+    end
+    rows << row
+end
+rows.each do |r|
+   puts r.join(' ')
+end
+# 求める出力結果
+# - L W
+# W - W
+# L L -
